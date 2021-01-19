92d7158a85b24ea9323c73ce2919317c4bd88131
GitHub-star-arch <63858220+GitHub-star-arch@users.noreply.github.com>
2021-01-19 20:14:20 +0530

    Add files via upload

---
+++ Screens/BookDonateScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class Donate extends React.Component {
+    render(){
+      return (
+        <View>
+            <Text>Sell</Text>
+        </View>
+      );
+    }
+  }
---
+++ Screens/BookRequestScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class Request extends React.Component {
+    render(){
+      return (
+        <View>
+            <Text>Buy</Text>
+        </View>
+      );
+    }
+  }
